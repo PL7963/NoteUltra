8241388de0e066fe83051eab2270afe3383bde28
SmallSun697 <smallsun698@gmail.com>
2024-12-09 16:58:41 +0800

    UI: Apply Material theme color to system bars in SettingsActivity

--- app/src/main/java/com/coolkie/noteultra/Settings.kt
+++ app/src/main/java/com/coolkie/noteultra/Settings.kt
@@ -3,6 +3,7 @@ package com.coolkie.noteultra
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,16 +23,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.core.view.WindowCompat
 import com.coolkie.noteultra.ui.theme.NoteUltraTheme
 
 class SettingsActivity : ComponentActivity() {
   @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
     setContent {
       NoteUltraTheme {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         Surface(
           color = MaterialTheme.colorScheme.background
         ) {
