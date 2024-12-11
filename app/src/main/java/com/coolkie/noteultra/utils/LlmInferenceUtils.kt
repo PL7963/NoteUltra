4ac372bde48cc634ce43f599a4a22640b5d7ba9a
PL7963 <peterlin1129@gmail.com>
2024-12-11 18:24:31 +0800

    Add LLM summary

--- app/src/main/java/com/coolkie/noteultra/utils/LlmInferenceUtils.kt
+++ app/src/main/java/com/coolkie/noteultra/utils/LlmInferenceUtils.kt
@@ -12,7 +12,7 @@ class LlmInferenceUtils(context: Context) {
     init {
         val options = LlmInference.LlmInferenceOptions.builder()
             .setModelPath(modelPath)
-            .setMaxTokens(1000)
+            .setMaxTokens(4096)
             .build()
 
         llmInference = LlmInference.createFromOptions(context, options)
@@ -20,10 +20,10 @@ class LlmInferenceUtils(context: Context) {
 
     fun answerUserQuestion(): String {
         val prompt =
-            "你是一位AI，你需要幫助User搜尋資訊，Context是使用者與其他人的對話內容，你需要根據Context內容回答問題，不得偏離問題，若Context資訊不足以回答問題，請拒絕回答問題\n"
+            "User: 請幫助User從文本中提取資訊，Context包含使用者與其他人的對話內容，你需要根據Context內容回答問題，不得偏離問題，若Context資訊不足以回答問題，請拒絕回答問題\n"
         val context =
-            "<start_of_turn>Context: 你要啥要買的嗎？額，可以幫我買高麗菜跟雞蛋嗎？好<end_of_turn>"
-        val toLlm = StringBuilder(prompt).append(context)
+            "Context: 你要啥要買的嗎？額，可以幫我買高麗菜跟雞蛋嗎？好"
+        val toLlm = StringBuilder("<start_of_turn>" + prompt + context + "<end_of_turn>")
         for (i in userQueryList.indices) {
             toLlm.append("<start_of_turn>User: ${userQueryList[i]}<end_of_turn>\n")
             toLlm.append(
@@ -33,6 +33,17 @@ class LlmInferenceUtils(context: Context) {
         }
         toLlm.append("<start_of_turn>User: ${userQueryList.last()}<end_of_turn>\n")
         toLlm.append("<start_of_turn>Assistant: ")
+        val response = llmInference.generateResponse(toLlm.toString())
+
+        return response
+    }
+
+    fun summaryResponse(): String {
+        val prompt = "User: 請簡化Context所提供的內容，整理成一份可讀性高的文字\n"
+        val context = "你在之前的對話中你提到你需要購買高麗菜跟雞蛋"
+        val toLlm = StringBuilder("<start_of_turn>" + prompt + context + "<end_of_turn>")
+        toLlm.append("<start_of_turn>Assistant: ")
+
         val response = llmInference.generateResponse(toLlm.toString())
 
         return response
