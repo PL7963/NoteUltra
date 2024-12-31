9f6aedbbf8a549572c64ff70c4762731cf142cb5
tusoar <obbi3391@gmail.com>
2024-12-31 11:29:04 +0800

    [+] Update : Change return id.

--- app/src/main/java/com/coolkie/noteultra/utils/VectorUtils.kt
+++ app/src/main/java/com/coolkie/noteultra/utils/VectorUtils.kt
@@ -1,5 +1,6 @@
 package com.coolkie.noteultra.utils
 
+import android.util.Log
 import com.coolkie.noteultra.data.ChatHistory
 import com.coolkie.noteultra.data.ChatHistory_
 import io.objectbox.Box
@@ -7,14 +8,16 @@ import java.time.LocalDate
 import java.time.LocalTime
 
 class VectorUtils(private val box: Box<ChatHistory>) {
-    fun search(date: Int, vector: FloatArray): List<Long> {
+    fun search(date: LocalDate, vector: FloatArray): List<String> {
+        val epochDay = date.toEpochDay().toInt()
         val query = box.query(
-            ChatHistory_.contentVector.nearestNeighbors(vector, 2)
-                .and(ChatHistory_.date.equal(date))
+            ChatHistory_.contentVector.nearestNeighbors(vector, 5)
+                .and(ChatHistory_.date.equal(epochDay))
         ).build()
 
-        val results = query.findIdsWithScores()
-        return results.map { it.id }
+        val results = query.find()
+        Log.d("VectorUtils", "[+] Found results: ${results.map { it.content }}")
+        return results.map { it.content }
     }
 
     fun store(date: LocalDate, time: LocalTime, content: String, vector: FloatArray) {
