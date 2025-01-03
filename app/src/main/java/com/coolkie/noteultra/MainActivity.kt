0ee839b7f80e5dd9b9362a7528f3be6364841d88
tusoar <obbi3391@gmail.com>
2025-01-03 20:16:10 +0800

    [+] Add : ObjectBox initialization feature.

--- app/src/main/java/com/coolkie/noteultra/MainActivity.kt
+++ app/src/main/java/com/coolkie/noteultra/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.material3.ExperimentalMaterial3Api
+import com.coolkie.noteultra.data.ChatHistory
+import com.coolkie.noteultra.data.MyObjectBox
 import com.coolkie.noteultra.data.NoteViewModel
 import com.coolkie.noteultra.data.NoteViewModelFactory
 import com.coolkie.noteultra.data.NotesDatabase
@@ -13,6 +15,7 @@ import com.coolkie.noteultra.ui.MainView
 import com.coolkie.noteultra.ui.theme.NoteUltraTheme
 import com.coolkie.noteultra.utils.EmbeddingUtils
 import com.coolkie.noteultra.utils.LlmInferenceUtils
+import io.objectbox.BoxStore
 
 class MainActivity : ComponentActivity() {
   private lateinit var llmInstance: LlmInferenceUtils
@@ -23,13 +26,18 @@ class MainActivity : ComponentActivity() {
 
   @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
+
+    // ObjectBox initialization
+    val boxStore: BoxStore = MyObjectBox.builder().androidContext(this).build()
+    val chatHistoryBox = boxStore.boxFor(ChatHistory::class.java)
+
     super.onCreate(savedInstanceState)
     llmInstance = LlmInferenceUtils(this)
     textEmbeddingUtils = EmbeddingUtils(this)
     enableEdgeToEdge()
     setContent {
       NoteUltraTheme {
-        MainView(llmInstance, noteViewModel)
+        MainView(llmInstance, chatHistoryBox, noteViewModel)
       }
     }
   }
