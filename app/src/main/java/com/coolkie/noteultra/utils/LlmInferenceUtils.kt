0af53cba00d415c691aec2a8ba19729634618b9f
SmallSun697 <smallsun698@gmail.com>
2025-01-08 19:59:35 +0800

    feat: Update Chat page LLM reply

--- app/src/main/java/com/coolkie/noteultra/utils/LlmInferenceUtils.kt
+++ app/src/main/java/com/coolkie/noteultra/utils/LlmInferenceUtils.kt
@@ -22,25 +22,25 @@ class LlmInferenceUtils(context: Context, vectorUtils: VectorUtils) {
     }
 
     fun answerUserQuestion(): String {
+        val prompt = "請試著用以下文本與USER交談，如果文本與USER無關請自行回答USER"
         val embeddedText = embeddingUtils.embedText(userQueryList.last())
-        var relatedResults = ""
-        if (embeddedText != null) {
-            relatedResults = vectorUtil.search(embeddedText).joinToString(separator = " ")
+        val relatedResults = embeddedText?.let { vectorUtil.search(it) }
+        val userQueryLast3 = userQueryList.takeLast(4).dropLast(1)
+        val userQuery = userQueryList.last()
+        val llmResponseLast3 = llmResponseList.takeLast(3)
+        val toLlm = StringBuilder().apply {
+            append("<start_of_turn>$prompt<end_of_turn>")
+            relatedResults?.forEach { result ->
+                append("<start_of_turn>$result<end_of_turn>")
+            }
+            userQueryLast3.zip(llmResponseLast3)
+                .forEach { (userQuery, llmResponse) ->
+                    append("<start_of_turn>$userQuery<end_of_turn>")
+                    append("<start_of_turn>$llmResponse<end_of_turn>")
+                }
+            append("<start_of_turn>USER: $userQuery<end_of_turn>")
+            append("<start_of_turn>Assistant:")
         }
-        val prompt =
-            "User: 請幫助User從文本中提取資訊，Context包含使用者與其他人的對話內容，你需要根據Context內容回答問題，不得偏離問題，若Context資訊不足以回答問題，請拒絕回答問題\n"
-        val context =
-            "Context: ${relatedResults}"
-        val toLlm = StringBuilder("<start_of_turn>" + prompt + context + "<end_of_turn>")
-        for (i in userQueryList.indices) {
-            toLlm.append("<start_of_turn>User: ${userQueryList[i]}<end_of_turn>\n")
-            toLlm.append(
-                "<start_of_turn>Assistant: ${
-                    llmResponseList.getOrNull(i)?.takeIf { it.isNotEmpty() } ?: "none"
-                }<end_of_turn>\n")
-        }
-        toLlm.append("<start_of_turn>User: ${userQueryList.last()}<end_of_turn>\n")
-        toLlm.append("<start_of_turn>Assistant: ")
         val response = llmInference.generateResponse(toLlm.toString())
 
         return response
