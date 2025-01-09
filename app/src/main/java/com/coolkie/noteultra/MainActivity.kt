c395ef5af461d05ee0e89483108a5c6d11cccaf8
SmallSun697 <smallsun698@gmail.com>
2025-01-09 16:25:24 +0800

    Fix: Repeated initialization

--- app/src/main/java/com/coolkie/noteultra/MainActivity.kt
+++ app/src/main/java/com/coolkie/noteultra/MainActivity.kt
@@ -6,46 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.material3.ExperimentalMaterial3Api
-import com.coolkie.noteultra.data.ChatHistory
-import com.coolkie.noteultra.data.MyObjectBox
 import com.coolkie.noteultra.data.NoteViewModel
 import com.coolkie.noteultra.data.NoteViewModelFactory
 import com.coolkie.noteultra.data.NotesDatabase
 import com.coolkie.noteultra.ui.MainView
 import com.coolkie.noteultra.ui.theme.NoteUltraTheme
-import com.coolkie.noteultra.utils.EmbeddingUtils
-import com.coolkie.noteultra.utils.LlmInferenceUtils
-import com.coolkie.noteultra.utils.VectorUtils
-import com.coolkie.noteultra.utils.asr.VoiceRecognition
-import io.objectbox.BoxStore
 
 class MainActivity : ComponentActivity() {
-  private lateinit var llmInstance: LlmInferenceUtils
-  private lateinit var textEmbeddingUtils: EmbeddingUtils
-  private lateinit var voiceRecognition: VoiceRecognition
-  private val noteViewModel: NoteViewModel by viewModels {
-    NoteViewModelFactory(NotesDatabase.getDatabase(applicationContext))
-  }
-
-  @OptIn(ExperimentalMaterial3Api::class)
-  override fun onCreate(savedInstanceState: Bundle?) {
-
-    // ObjectBox initialization
-    val boxStore: BoxStore = MyObjectBox.builder().androidContext(this).build()
-    val chatHistoryBox = boxStore.boxFor(ChatHistory::class.java)
-    val vectorUtils = VectorUtils(chatHistoryBox)
-
-    super.onCreate(savedInstanceState)
-    llmInstance = LlmInferenceUtils(this,vectorUtils)
-    textEmbeddingUtils = EmbeddingUtils(this)
-    voiceRecognition = VoiceRecognition(this, vectorUtils, textEmbeddingUtils)
-    voiceRecognition.initModel(this)
-    voiceRecognition.startRecording(this)
-    enableEdgeToEdge()
-    setContent {
-      NoteUltraTheme {
-        MainView(llmInstance, vectorUtils, noteViewModel)
-      }
+    private val noteViewModel: NoteViewModel by viewModels {
+        NoteViewModelFactory(NotesDatabase.getDatabase(applicationContext))
     }
-  }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val app = application as NoteUltraApp
+        val llmInstance = app.llmInstance
+        val textEmbeddingUtils = app.textEmbeddingUtils
+        val vectorUtils = app.vectorUtils
+
+        enableEdgeToEdge()
+        setContent {
+            NoteUltraTheme {
+                MainView(llmInstance, vectorUtils, noteViewModel)
+            }
+        }
+    }
 }
--- app/src/main/java/com/coolkie/noteultra/NoteUltraApp.kt
+++ app/src/main/java/com/coolkie/noteultra/NoteUltraApp.kt
@@ -4,7 +4,33 @@ import android.app.Application
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import com.coolkie.noteultra.data.ChatHistory
+import com.coolkie.noteultra.data.MyObjectBox
+import com.coolkie.noteultra.utils.EmbeddingUtils
+import com.coolkie.noteultra.utils.LlmInferenceUtils
+import com.coolkie.noteultra.utils.VectorUtils
+import com.coolkie.noteultra.utils.asr.VoiceRecognition
+import io.objectbox.BoxStore
 
 class NoteUltraApp : Application() {
-  val dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+    val boxStore: BoxStore by lazy {
+        MyObjectBox.builder().androidContext(this).build()
+    }
+    val dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+    val vectorUtils by lazy {
+        val chatHistoryBox = boxStore.boxFor(ChatHistory::class.java)
+        VectorUtils(chatHistoryBox)
+    }
+    val llmInstance by lazy {
+        LlmInferenceUtils(this, vectorUtils)
+    }
+    val textEmbeddingUtils by lazy {
+        EmbeddingUtils(this)
+    }
+    val voiceRecognition by lazy {
+        VoiceRecognition(this, vectorUtils, textEmbeddingUtils).apply {
+            initModel(this@NoteUltraApp)
+            startRecording(this@NoteUltraApp)
+        }
+    }
 }
