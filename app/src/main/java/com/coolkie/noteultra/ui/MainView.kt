f1df5bc2c3e97a5896c04322b40eb7ed3492b266
SmallSun697 <smallsun698@gmail.com>
2025-01-20 19:40:36 +0800

    Fix: Edge to edge bottom navigation bar overlap

--- app/src/main/java/com/coolkie/noteultra/ui/MainView.kt
+++ app/src/main/java/com/coolkie/noteultra/ui/MainView.kt
@@ -5,11 +5,16 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -178,7 +183,7 @@ fun MainView(
         )
       },
       bottomBar = {
-        Box(
+        Column(
           modifier = Modifier
             .fillMaxWidth()
             .heightIn(56.dp)
@@ -235,6 +240,12 @@ fun MainView(
                 )
               }
             }
+          )
+          Spacer(
+            modifier = Modifier
+              .windowInsetsBottomHeight(
+                WindowInsets.systemBars
+              )
           )
         }
       }
