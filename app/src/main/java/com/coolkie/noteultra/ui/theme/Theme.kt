c310dedf6184c168dd588a4fc78995eb22e77d0f
SmallSun697 <smallsun698@gmail.com>
2025-02-19 20:15:10 +0800

    Add: Integrate themes with dataStore

--- app/src/main/java/com/coolkie/noteultra/ui/theme/Theme.kt
+++ app/src/main/java/com/coolkie/noteultra/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.coolkie.noteultra.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,7 +8,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import com.coolkie.noteultra.data.DarkMode
+import com.coolkie.noteultra.data.SettingsRepository
+import com.coolkie.noteultra.data.dataStore
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -35,14 +39,22 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun NoteUltraTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val context = LocalContext.current
+    val settingsRepository = remember(context) { SettingsRepository(context.dataStore) }
+    val darkModeState = settingsRepository.darkModeFlow.collectAsState(DarkMode.SYSTEM)
+    val darkMode = darkModeState.value
+    val darkTheme = when (darkMode) {
+        DarkMode.ENABLED -> true
+        DarkMode.DISABLED -> false
+        DarkMode.SYSTEM -> isSystemInDarkTheme()
+    }
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
